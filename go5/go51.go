483347c4345ac8d7985fdc3340bb3c47640b7a28
BlackNoxis <steven.darklight@gmail.com>
2013-06-21 18:45:19 +0300

    Expanded the program, again having some fun

--- go5/go51.go
+++ go5/go51.go
@@ -8,14 +8,30 @@ import (
 	"strings"
 )
 
+func whatever(a string) string {
+        cmd := exec.Command("tr", "a-z", "A-Z")
+        cmd.Stdin = strings.NewReader("mother")
+        var out bytes.Buffer
+        cmd.Stdout = &out
+        err := cmd.Run()
+        if err != nil {
+                log.Fatal(err)
+        }
+	a = out.String()
+        //fmt.Printf("in all caps: %q\n", out.String())
+	//fmt.Printf("in all caps: %q\n", a)
+	return a
+}
+
 func main() {
-	cmd := exec.Command("tr", "a-z", "A-Z")
-	cmd.Stdin = strings.NewReader("help")
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("in all caps: %q\n", out.String())
+	//cmd := exec.Command("tr", "a-z", "A-Z")
+	//cmd.Stdin = strings.NewReader("mother")
+	//var out bytes.Buffer
+	//cmd.Stdout = &out
+	//err := cmd.Run()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Printf("in all caps: %q\n", out.String())
+	fmt.Println("The requested main function shows this:", whatever("any string here") )
 }
