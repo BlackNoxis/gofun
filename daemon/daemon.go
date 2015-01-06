7ae7028b882e8230a03a53c875a1c4c8431e03ea
BlackNoxis <steven.darklight@gmail.com>
2015-01-06 23:32:49 +0200

    Adding a daemon example. The function looks for the specified folder/file if it exists. If it does, it responds at the specified time that it works. If it doesn`t, it terminates the function (thus, press enter to stop it definitely)

---
+++ daemon/daemon.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"os"
+	"math/rand"
+)
+
+func thename(name chan string) {
+    for i := 0; ; i++ {
+        name <- "/home"
+    }
+}
+
+func kreator(name chan string) bool {
+	for {
+		if _, err := os.Stat(<-name); err != nil {
+		if os.IsNotExist(err) {
+			fmt.Printf("Does not work\n")
+			return false
+	   }
+		}
+		whenever := time.Duration(rand.Intn(250))
+		time.Sleep(time.Millisecond * whenever)
+	fmt.Printf("Works\n")
+	}
+	return true
+}
+
+func main() {
+	var name chan string = make(chan string)
+
+	go thename(name)
+	go kreator(name)
+
+    var input string
+    fmt.Scanln(&input)
+}
